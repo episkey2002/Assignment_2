0b750144307d093e57d0ad1f8794b658f94b5710
episkey2002 <pritirajunikam2411@gmail.com>
2023-09-27 22:59:12 +0530

    Updated Assignment 2

---
+++ Q3.js
@@ -0,0 +1,7 @@
+let numericVar = 42;
+let stringVar = "Hello World";
+let booleanVar = true;
+
+console.log("Type of numericVar:", typeof numericVar);
+console.log("Type of stringVar:", typeof stringVar);
+console.log("Type of booleanVar:", typeof booleanVar);
---
+++ Q5.js
@@ -0,0 +1,5 @@
+let str1 = "Hello";
+let str2 = "World";
+
+let concatenateString = str1 + str2;
+console.log(concatenateString);
